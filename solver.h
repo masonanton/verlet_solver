12c40dd58d3cccc415da1e4fa8f7a2b300f9a8af
masonanton <masonanton1@gmail.com>
2025-01-14 23:13:09 -0600

    Update solver method

--- solver.h
+++ solver.h
@@ -6,6 +6,7 @@
 #define SOLVER_H
 
 #include "engine/engine.hpp"
+#include "game/physics/object.hpp"
 
 
 
@@ -28,6 +29,46 @@ struct VerletObject {
     void accelerate(Vec2 acc) {
         acceleration += acc;
     }
+};
+
+struct Solver {
+  Vec2 gravity = {0.0f, 1000.0f};
+
+  void update(float dt)
+  {
+    applyGravity();
+    applyConstraint();
+    updatePositions(dt);
+  }
+
+  void updatePositions(float dt)
+  {
+    core::foreach<VerletObject>([](VerletObject& obj) {
+      obj.updatePosition(dt);
+    });
+  }
+
+  void applyGravity()
+  {
+    core::foreach<VerletObject>([this](VerletObject& obj) {
+      obj.accelerate(gravity);
+    });
+  }
+
+  void applyConstraint()
+  {
+    const Vec2 position{800.0f, 450.0f};
+    const float radius = 400.0f;
+    core::foreach<VerletObject>([&](VerletObject& obj) {
+      const Vec2 to_obj = obj.position_current - position;
+      const float dist = MathVec2::length(to_obj);
+      // 50 is the default radius
+      if (dist > radius - 50.0f) {
+        const Vec2 n = to_obj / dist;
+        obj.position_current = position + n * (dist - 50.0f);
+      }
+    });
+  }
 
 };
 
